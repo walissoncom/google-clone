60d0df0ad5c2bb086effe33fc933fd7c8289eea6
Walisson Rodrigues <contact@walisson.com>
2020-12-20 16:32:25 +1100

    Importing MaterialUI and React Router Dom + Setting routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      <h1>Google Clone</h1>
+
+      <Router>
+        <Switch>
+
+          {/* Home (the one with the search on) */}
+          <Route path="/" exact>
+            <h1>Home</h1>
+          </Route>
+
+          {/* SearchPage (The results page) */}
+          <Route path="/search">
+            <h1>This is the search page</h1>
+          </Route>
+
+        </Switch>
+      </Router>
     </div>
   );
 }
