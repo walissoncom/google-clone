ecebc3280ef42472a075c32c21e9f0526277c887
Walisson Rodrigues <contact@walisson.com>
2020-12-20 19:46:53 +1100

    Context API - Data Layer implementation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import reducer, { initialState } from './reducer';
+import { StateProvider } from './StateContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -29,7 +29,7 @@ function Home() {
                 <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt="Logo" />
 
                 <div className="home__inputContainer">
-                    <Search hideButtons />
+                    <Search />
                 </div>
             </div>
         </div>
